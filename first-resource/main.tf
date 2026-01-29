916aa161a0c2e2a2d4577b0e13a124bf6c0fc86d
bindusdet <bindu.sdet65@gmail.com>
2026-01-29 12:48:49 +0530

    first commit

---
+++ first-resource/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+   region = "us-west-2"
+   access_key = ""
+   secret_key = ""
+}
+
+resource "aws_vps" "myvpc"{
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = Terraform-VPC"
+  }
+}
