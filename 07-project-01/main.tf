f053803eac8f2b88ca51561990e060e29ab672a2
bindusdet <bindu.sdet65@gmail.com>
2026-01-29 22:25:17 +0530

    changes

--- 07-project-01/main.tf
+++ 07-project-01/main.tf
@@ -1,79 +1,70 @@
-#provider
-#vpc creation
-#subne-1
-#subnet-2
-#route table
-#internet gateway
-#route tabel association 
-
-
-
+# provider
 provider "aws" {
-     region = "us-west-2"
+  region = "us-west-2"
 }
 
+# vpc creation
 resource "aws_vpc" "myvpc" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_support = true
-    enable_dns_hostnames = true
-    tags = {
-        Name = "Terraform_vpc"
-    }
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags = {
+    Name = "vpc-project-by-terraform"
+  }
 }
-
-resource "aws_subnet" "mysubnet-1" {
+# subnet-1
+resource "aws_subnet" "subnet-1" {
     vpc_id = aws_vpc.myvpc.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "us-west-2a"
     map_public_ip_on_launch = true
     tags = {
-        Name = "Terraform_subnet1"
-    }  
-}     
+      Name = "subnet-1-project-by-terraform"
+    }
+  }
+  
 
-resource "aws_subnet" "mysubnet-2" {
+
+# subnet-2
+resource "aws_subnet" "subnet-2" {
     vpc_id = aws_vpc.myvpc.id
     cidr_block = "10.0.2.0/24"
     availability_zone = "us-west-2b"
     map_public_ip_on_launch = true
     tags = {
-        Name = "Terraform_subnet2"
-    }   
-}   
-
-#route table
-resource "aws_route_table" "myroute-table-public" {
+      Name = "subnet-2-project-by-terraform"
+    }
+  }
+  
+# route table
+resource "aws_route_table" "public_route_table" {
     vpc_id = aws_vpc.myvpc.id
     tags = {
-        Name = "Terraform_route-table"
+      Name = "public-route-table-project-by-terraform"
     }
-}     
-
-#internet gateway
+  }
+  
+# Internet gateway 
 resource "aws_internet_gateway" "myigw" {
     vpc_id = aws_vpc.myvpc.id
     tags = {
-        Name = "Terraform_igw"
+        Name = "MyIGW"
     }
-}  
-
-#route 
-resource "aws_route" "route-to-internet" {
-    route_table_id = aws_route_table.myroute-table-public.id
-    destination_cidr_block = "10.0.0.0/0"
-    gateway_id = aws_internet_gateway.myigw.id
+}
+# Route 
+resource "aws_route" "public_route" {
+  route_table_id = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.myigw.id
 }
 
 
-#route table association
+# Route table association
+
 resource "aws_route_table_association" "public_association" {
-    subnet_id = aws_subnet.mysubnet-1.id    
-    route_table_id = aws_route_table.myroute-table-public.id
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id = aws_subnet.subnet-1.id
 }
-
-
-
-
 
 
 
